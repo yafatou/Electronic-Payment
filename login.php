f6b90169686a90cf0593696f97905a4128d53764
yafatou <63559350+yafatou@users.noreply.github.com>
2020-06-05 04:47:54 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Login And Registratin</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	
+</head>
+<body>
+	<div class="container">
+		<div class="login-box">
+		<div class="row">
+			<div class="col-md-6 login-left">
+				<h2> Login Here </h2>
+				<form action="validation.php" method="post">
+					<div class="form-group">
+						<label> Username </label>
+						<input type="text" name="User" class="form-control" required>
+					</div>
+					
+					<div class="form-group">
+						<label> Password </label>
+						<input type="password" name="password" class="form-control" required>
+					</div>
+
+					<button type="Submit" class="btn btn-primary"> Login </button>
+				</form>
+				
+			</div>
+
+			<div class="col-md-6 login-right">
+				<h2> Register Here </h2>
+				<form action="registration.php" method="post">
+					<div class="form-group">
+						<label> Username </label>
+						<input type="text" name="User" class="form-control" required>
+					</div>
+					
+					<div class="form-group">
+						<label> Password </label>
+						<input type="password" name="password" class="form-control" required>
+					</div>
+
+					<button type="Submit" class="btn btn-primary"> Register </button>
+				</form>
+				
+			</div>
+			
+		</div>
+		
+</div>
+
+	</div>
+
+</body>
+</html>
